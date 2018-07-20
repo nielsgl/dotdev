2c6a61fc9437bbde523d6cb1ef2557d922799a2f
Niels van Galen Last <nvangalenlast@gmail.com>
2018-07-20 19:02:49 +0200

    updated node scripts
    Signed-off-by: Niels van Galen Last <nvangalenlast@gmail.com>

--- node/path.zsh
+++ node/path.zsh
@@ -4,14 +4,27 @@ __nodenv_started=0
 
 __nodenv_init() {
 	test $__nodenv_started = 0 && {
-        echo "exec nodenv init"
 		eval "$(command nodenv init -)"
 		__nodenv_started=1
 	}
 }
 
+nodenv() {
+    __nodenv_init
+    command nodenv "$@"
+}
+
 node() {
-    echo "node command"
     __nodenv_init
     command node "$@"
+}
+
+npm() {
+    __nodenv_init
+    command npm "$@"
+}
+
+yarn() {
+    __nodenv_init
+    command yarn "$@"
 }
